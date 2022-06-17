bb10f5925100b278ff71889cf2f9d0063d4e60f6
Adrian Eguez <eadepto@hotmail.com>
2022-06-17 18:12:48 +0200

    Hasta ciclo vida

--- 01 - android/app/src/main/java/com/example/movcompvaes2022a/MainActivity.kt
+++ 01 - android/app/src/main/java/com/example/movcompvaes2022a/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.movcompvaes2022a
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import io.sentry.Sentry
 import io.sentry.SentryLevel
 
@@ -11,5 +14,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //Sentry.captureMessage("testing SDK setup", SentryLevel.INFO)
         Sentry.captureMessage("testing SDK setup", SentryLevel.INFO)
+
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida
+            .setOnClickListener {
+                irActividad(ACicloVida::class.java)
+            }
+
+//        val botonCicloVida2 = findViewById<TextView>(R.id.btn_ciclo_vida)
+//        botonCicloVida2.text = "HOLA"
     }
+
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
+    }
+
 }
