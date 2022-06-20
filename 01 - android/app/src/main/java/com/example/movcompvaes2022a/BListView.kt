ec00e7159d31c43c030b7fba593de3313210e39a
Adrian Eguez <eadepto@hotmail.com>
2022-06-20 19:51:18 +0200

    Hasta dialogo

--- 01 - android/app/src/main/java/com/example/movcompvaes2022a/BListView.kt
+++ 01 - android/app/src/main/java/com/example/movcompvaes2022a/BListView.kt
@@ -1,18 +1,26 @@
 package com.example.movcompvaes2022a
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
 
 class BListView : AppCompatActivity() {
-    val arreglo: ArrayList<Int> = arrayListOf(1, 2, 3, 4, 5)
+//    val arreglo: ArrayList<Int> = arrayListOf(1, 2, 3, 4, 5)
+    val arreglo: ArrayList<BEntrenador> = BBaseDatosMemoria.arregloBEntrenador
+
+    var idItemSeleccionado = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blist_view)
-
         val listView = findViewById<ListView>(R.id.lv_list_view)
         val adaptador = ArrayAdapter(
             this, // Contexto
@@ -21,22 +29,93 @@ class BListView : AppCompatActivity() {
         )
         listView.adapter = adaptador
         adaptador.notifyDataSetChanged()
-
         val botonAnadirListView = findViewById<Button>(
             R.id.btn_anadir_list_view)
-
         botonAnadirListView
             .setOnClickListener {
                 anadirNumero(adaptador)
             }
+        registerForContextMenu(listView)
+    }
 
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        // Llenamos las opciones del menu
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        // Obtener el id del ArrayListSeleccionado
+        val info = menuInfo as AdapterView.AdapterContextMenuInfo
+        val id = info.position
+        idItemSeleccionado = id
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.mi_editar ->{
+                "${idItemSeleccionado}"
+                return true
+            }
+            R.id.mi_eliminar ->{
+                abrirDialogo()
+                "${idItemSeleccionado}"
+                return true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+    fun abrirDialogo(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Desea eliminar")
+        builder.setPositiveButton(
+            "Aceptar",
+            DialogInterface.OnClickListener { dialog, which ->
+                // Al Aceptar eliminar el registro
+            }
+        )
+        builder.setNegativeButton(
+                "Cancelar",
+        null
+        )
+
+        val opciones = resources.getStringArray(
+            R.array.string_array_opciones_dialogo
+        )
+        val seleccionPrevia = booleanArrayOf(
+            true, // Lunes seleccionado
+            false, // Martes NO seleccionado
+            false // Miercoles NO seleccionado
+        )
+        builder.setMultiChoiceItems(
+            opciones,
+            seleccionPrevia,
+            { dialog,
+              which,
+              isChecked ->
+                "Dio clic en el item ${which}"
+            }
+        )
+        val dialogo = builder.create()
+        dialogo.show()
 
     }
 
+
+
+
+
+
     fun anadirNumero(
-    adaptador: ArrayAdapter<Int>
+//    adaptador: ArrayAdapter<Int>
+    adaptador: ArrayAdapter<BEntrenador>
     ){
-        arreglo.add(1)
+        arreglo.add(
+            BEntrenador("Vicente", "Eguez")
+        )
         adaptador.notifyDataSetChanged()
     }
 }
